f98f963fbd40ce817dfb3a5ab387f5a9bbf11ca9
gsuke <54441258+gsuke@users.noreply.github.com>
2022-06-21 15:25:33 +0900

    fix: change file names

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import { FaGithub } from 'react-icons/fa';
 
-export default function MenuBar() {
+export default function NavBar() {
   return (
     <nav className="bg-base-300 text-base-content flex sticky top-0 justify-between items-center p-2 mb-1">
       <Link href="/">
