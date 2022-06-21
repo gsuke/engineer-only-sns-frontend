9274ff84534928aaa440e1bfb98d908e5daa1f50
gsuke <54441258+gsuke@users.noreply.github.com>
2022-06-21 11:11:10 +0900

    feat: create NavBar

---
+++ components/nav-bar.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import { FaGithub } from 'react-icons/fa';
+
+export default function MenuBar() {
+  return (
+    <nav className="bg-neutral flex sticky -top-0 flex-row justify-between p-2 mb-2">
+      <Link href="/">
+        <a className="btn text-2xl">Engineer Only SNS</a>
+      </Link>
+      <a
+        href="https://github.com/gsuke/engineer-only-sns-frontend"
+        target="_blank"
+        className="btn btn-circle"
+        rel="noreferrer"
+      >
+        <FaGithub size={40} />
+      </a>
+    </nav>
+  );
+}
