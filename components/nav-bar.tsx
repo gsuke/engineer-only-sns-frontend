d90e7ccbef99c13f2a3e69bb3142469bbdd90212
gsuke <54441258+gsuke@users.noreply.github.com>
2022-06-21 12:06:32 +0900

    fix: minor adjustment

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -3,14 +3,13 @@ import { FaGithub } from 'react-icons/fa';
 
 export default function MenuBar() {
   return (
-    <nav className="bg-neutral flex sticky -top-0 flex-row justify-between p-2 mb-1">
+    <nav className="bg-base-300 text-base-content flex sticky -top-0 flex-row justify-between items-center p-2 mb-1">
       <Link href="/">
-        <a className="btn text-2xl normal-case">Engineer Only SNS</a>
+        <a className="text-3xl font-bold normal-case">Engineer Only SNS</a>
       </Link>
       <a
         href="https://github.com/gsuke/engineer-only-sns-frontend"
         target="_blank"
-        className="btn btn-circle"
         rel="noreferrer"
       >
         <FaGithub size={40} />
