50833d8c73e3e3950d0328555e5d05f1d9dc5d00
gsuke <54441258+gsuke@users.noreply.github.com>
2022-06-27 16:34:23 +0900

    feat: add feature to skip duplicated texts

--- components/TextList.tsx
+++ components/TextList.tsx
@@ -6,9 +6,15 @@ type Props = {
 };
 
 export default function TextList({ texts }: Props) {
+  // 新規投稿がある場合、textが重複して読み込まれるので、それらを除去する。
+  const uniqueTexts = Array.from(
+    // text.idをkeyとしたMapを作り、valuesを取ると重複を除去できる。
+    new Map(texts.map((text) => [text.id, text])).values(),
+  );
+
   return (
     <>
-      {texts.map((text) => (
+      {uniqueTexts.map((text) => (
         <TextComponent key={text.id} text={text} />
       ))}
     </>
