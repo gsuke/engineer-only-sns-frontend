3ee4285ecaaff68745fe612a061487eec751d23f
gsuke <54441258+gsuke@users.noreply.github.com>
2022-06-27 21:03:16 +0900

    refactor: move paging logic to TextList

--- components/TextList.tsx
+++ components/TextList.tsx
@@ -1,11 +1,24 @@
-import type Text from '../lib/models/Text';
+import { useEffect } from 'react';
 import TextComponent from './Text';
+import useNewTexts from '../hooks/useNewTexts';
+import useTexts from '../hooks/useTexts';
 
-type Props = {
-  texts: Text[];
-};
+export default function TextList() {
+  const oldTexts = useTexts();
+  const newTexts = useNewTexts(oldTexts.texts);
 
-export default function TextList({ texts }: Props) {
+  // 新規投稿が溜まった場合は1ページに戻し、再読み込みをかける
+  useEffect(() => {
+    void (async () => {
+      if (newTexts.isTooManyTexts) {
+        await oldTexts.setSize(1);
+        await oldTexts.mutate();
+      }
+    })();
+  }, [newTexts.isTooManyTexts, oldTexts]);
+
+  const texts = [...newTexts.texts, ...oldTexts.texts];
+
   // 新規投稿がある場合、textが重複して読み込まれるので、それらを除去する。
   const uniqueTexts = Array.from(
     // text.idをkeyとしたMapを作り、valuesを取ると重複を除去できる。
@@ -13,10 +26,23 @@ export default function TextList({ texts }: Props) {
   );
 
   return (
-    <>
+    <main className="m-2 w-full max-w-xl">
       {uniqueTexts.map((text) => (
         <TextComponent key={text.id} text={text} />
       ))}
-    </>
+
+      {/* もっと読み込むボタン */}
+      {!oldTexts.isLoadingMore && (
+        <button
+          type="button"
+          className="btn"
+          onClick={async () => {
+            await oldTexts.setSize(oldTexts.size + 1);
+          }}
+        >
+          もっと読み込む
+        </button>
+      )}
+    </main>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,13 @@
-import { useEffect } from 'react';
 import NavBar from '../components/NavBar';
 import PostForm from '../components/PostForm';
 import TextList from '../components/TextList';
-import useNewTexts from '../hooks/useNewTexts';
-import useTexts from '../hooks/useTexts';
 
 export default function Home() {
-  const useTextsResult = useTexts();
-  const useNewTextsResult = useNewTexts(useTextsResult.texts);
-
-  // 新規投稿が溜まった場合は1ページに戻し、再読み込みをかける
-  useEffect(() => {
-    void (async () => {
-      if (useNewTextsResult.isTooManyTexts) {
-        await useTextsResult.setSize(1);
-        await useTextsResult.mutate();
-      }
-    })();
-  }, [useNewTextsResult.isTooManyTexts, useTextsResult]);
-
-  const allTexts = [...useNewTextsResult.texts, ...useTextsResult.texts];
-
   return (
     <div>
       <NavBar />
       <div className="flex justify-center">
-        <main className="m-2 w-full max-w-xl">
-          {useTextsResult.texts && <TextList texts={allTexts} />}
-
-          {/* もっと読み込むボタン */}
-          {!useTextsResult.isLoadingMore && (
-            <button
-              type="button"
-              className="btn"
-              onClick={async () => {
-                await useTextsResult.setSize(useTextsResult.size + 1);
-              }}
-            >
-              もっと読み込む
-            </button>
-          )}
-        </main>
+        <TextList />
       </div>
       <PostForm />
     </div>
