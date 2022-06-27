f5cc2724c83d0462de9b835880d371fa5125ce97
gsuke <54441258+gsuke@users.noreply.github.com>
2022-06-27 15:29:33 +0900

    style: remove unnecessary comment

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,6 +1,5 @@
 import { apiUrl } from './const';
 
-// TODO: 404でエラーになるので、その対応をする
 const fetcher = async (endpoint: string) => {
   const response = await fetch(`${apiUrl}${endpoint}`);
   if (response.status === 404) {
