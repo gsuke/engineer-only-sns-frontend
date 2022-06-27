243f1715d58b45b4961734824d6b1e8ba55db16a
gsuke <54441258+gsuke@users.noreply.github.com>
2022-06-27 18:18:55 +0900

    feat: add feature to get new texts periodically

--- lib/const.ts
+++ lib/const.ts
@@ -3,3 +3,6 @@ export const apiUrl = 'https://versatileapi.herokuapp.com/api';
 
 // 1ページでフェッチする投稿の数
 export const textCountPerPage = 2;
+
+// 再読み込み時間(ミリ秒)
+export const refreshInterval = 30 * 1000;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,21 @@
 import NavBar from '../components/NavBar';
 import PostForm from '../components/PostForm';
 import TextList from '../components/TextList';
+import useNewTexts from '../hooks/useNewTexts';
 import useTexts from '../hooks/useTexts';
 
 export default function Home() {
   const useTextsResult = useTexts();
+  const useNewTextsResult = useNewTexts(useTextsResult.texts);
+
+  const allTexts = [...useNewTextsResult.texts, ...useTextsResult.texts];
 
   return (
     <div>
       <NavBar />
       <div className="flex justify-center">
         <main className="m-2 w-full max-w-xl">
-          {useTextsResult.texts && <TextList texts={useTextsResult.texts} />}
+          {useTextsResult.texts && <TextList texts={allTexts} />}
 
           {/* もっと読み込むボタン */}
           {!useTextsResult.isLoadingMore && (
