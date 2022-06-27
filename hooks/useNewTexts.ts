91571cc2021e7416feb576bd2a860618ac792a91
gsuke <54441258+gsuke@users.noreply.github.com>
2022-06-27 22:56:40 +0900

    refactor: remove unnecessary argument

--- hooks/useNewTexts.ts
+++ hooks/useNewTexts.ts
@@ -3,8 +3,11 @@ import type { SWRConfiguration } from 'swr';
 import fetcher from '../lib/fetcher';
 import type Text from '../lib/models/Text';
 import { refreshInterval, textCountPerPage } from '../lib/const';
+import useTexts from './useTexts';
 
-export default function useNewTexts(oldTexts: Text[]) {
+export default function useNewTexts() {
+  const oldTexts = useTexts();
+
   const config: SWRConfiguration = { refreshInterval };
 
   const { data, error } = useSWR<Text[], Error>(
@@ -17,7 +20,7 @@ export default function useNewTexts(oldTexts: Text[]) {
   const newTextsMap = new Map(data?.map((text) => [text.id, text]));
 
   // 古い投稿と重複するものは消していく
-  oldTexts.forEach((oldText) => {
+  oldTexts.texts.forEach((oldText) => {
     newTextsMap.delete(oldText.id);
   });
 
