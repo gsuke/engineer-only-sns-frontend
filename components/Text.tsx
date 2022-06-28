f9a75f95f89387f47eda49db406e4254bf27fc65
gsuke <54441258+gsuke@users.noreply.github.com>
2022-06-29 00:02:02 +0900

    refactor: add UserAvatar component

--- components/Text.tsx
+++ components/Text.tsx
@@ -1,6 +1,7 @@
 import Avatar from 'boring-avatars';
 import { format, parseISO } from 'date-fns';
 import type TextType from '../lib/models/Text';
+import UserAvatar from './UserAvatar';
 import UserName from './UserName';
 
 type Props = {
@@ -13,12 +14,7 @@ export default function Text({ text }: Props) {
   return (
     <article className="bg-base-300 text-base-content flex justify-start p-2 my-2 w-full rounded-md transition duration-100">
       <div className="mr-2">
-        <Avatar
-          size={64}
-          name={text._user_id}
-          variant="beam"
-          colors={['#92A1C6', '#146A7C', '#F0AB3D', '#C271B4', '#C20D90']}
-        />
+        <UserAvatar userId={text._user_id} size={64} />
       </div>
       <div className="grow">
         <div className="flex justify-between">
