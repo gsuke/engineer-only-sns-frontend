21c770b406ce7df7b3273a17605a72935e983489
gsuke <54441258+gsuke@users.noreply.github.com>
2022-06-28 18:39:22 +0900

    feat: add feature of infinite scroll

--- lib/const.ts
+++ lib/const.ts
@@ -2,7 +2,7 @@
 export const apiUrl = 'https://versatileapi.herokuapp.com/api';
 
 // 1ページでフェッチする投稿の数
-export const textCountPerPage = 2;
+export const textCountPerPage = 10;
 
 // 再読み込み時間(ミリ秒)
 export const refreshInterval = 30 * 1000;
