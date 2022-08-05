2ba6a6295d4f2b13a30999e03b8528b789bd99b8
gsuke <54441258+gsuke@users.noreply.github.com>
2022-08-05 11:07:28 +0900

    fix: add x-axis margin to article container

--- components/Container.tsx
+++ components/Container.tsx
@@ -5,5 +5,9 @@ type Props = {
 };
 
 export default function Container({ children }: Props) {
-  return <main className="my-2 mx-auto w-full max-w-xl">{children}</main>;
+  return (
+    <main className="my-2 mx-auto w-full max-w-xl">
+      <div className="mx-2">{children}</div>
+    </main>
+  );
 }
